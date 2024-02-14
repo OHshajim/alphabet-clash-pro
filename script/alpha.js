c621b9ae89ab234301343b12efaeaf266ee5d37f
OHshajim <ajshajimmax@gmail.com>
2024-02-15 05:29:14 +0600

    work on play ground

--- script/alpha.js
+++ script/alpha.js
@@ -1,4 +1,19 @@
-function play(){
-    removeElement('screen-sec')
-    addElement('playground')
+function continueGame() {
+    // get letter
+    const letter = getLetter();
+    // letter exchange
+    const ID = document.getElementById('Letter')
+    ID.innerText = letter;
+    // hi-light the letter
+    AddHiLight(letter);
+}
+
+function play() {
+    // remove  screen
+    removeElement('screen-sec');
+    // add play ground section
+    addElement('playground');
+
+    // letter related 
+    continueGame();
 };
--- script/multi.js
+++ script/multi.js
@@ -8,4 +8,23 @@ function removeElement(ID){
  function addElement(Id){
      const Element = document.getElementById(Id);
     Element.classList.remove('hidden');
- }
+ };
+//  add hight color and  remove hight color
+
+function AddHiLight(id){
+    const Element = document.getElementById(id);
+    Element.classList.add('bg-cyan-600')
+}
+function removeHiLight(id){
+    const Element = document.getElementById(id);
+    Element.classList.remove('bg-cyan-600')
+}
+
+//  gat a letter
+ function getLetter(){
+    const letterStr = "qwertyuiopasdfghjklzxcvbnm/";
+    const letters = letterStr.split('');
+    const number = Math.random()*26;
+    const index =Math.round(number);
+    return letters[index];
+ };
